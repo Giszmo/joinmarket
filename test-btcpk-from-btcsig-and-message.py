570da76fa34c67ea7862a7af4d798b11eb9a9dd2
Leo Wandersleb <leo@leowandersleb.de>
2015-11-19 15:01:45 -0300

    problem solved. we just send the public key with the sig. here is just how it works for reference

--- test-btcpk-from-btcsig-and-message.py
+++ test-btcpk-from-btcsig-and-message.py
@@ -4,17 +4,24 @@ sys.path.insert(0, os.path.join(data_dir, 'lib'))
 import bitcoin as btc
 import binascii
 
-btc_sk = 'L5cFx7NvWSWidwNtKnBQw5VrguTax2dGYajuz12S81UhX2ewRehH'
+btc_sk = btc.b58check_to_bin('L5cFx7NvWSWidwNtKnBQw5VrguTax2dGYajuz12S81UhX2ewRehH')
 data = 'hello world' # something to sign
-btc_sig = btc.ecdsa_sign(data, btc.b58check_to_bin(btc_sk))
+btc_sig = btc.ecdsa_sign(data, btc_sk)
 
 # send stuff to server, without sending the btc_pk
 
-btc_pk = '0000' # TODO: figure out btc_pk from only the btc_sig
+# TODO: figure out btc_pk from only the btc_sig
+btc_pk = btc.privkey_to_pubkey(btc_sk)
+
+print('the sk is {0}', btc_sk)
+print('the pk is {0}', btc_pk)
+print('the data is {0}', data)
+
 
 if not btc.ecdsa_verify(
         data,
-        btc_sig):
+        btc_sig,
+        btc_pk):
     print('failed')
 else:
     print('success')
