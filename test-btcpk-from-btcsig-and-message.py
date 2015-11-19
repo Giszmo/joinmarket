0989dc9b771a57867266fae2c1bd8f871a6919ba
Leo Wandersleb <leo@leowandersleb.de>
2015-11-19 15:46:19 -0300

    slightly nicer test

--- test-btcpk-from-btcsig-and-message.py
+++ test-btcpk-from-btcsig-and-message.py
@@ -1,27 +1,19 @@
-import sys, os
+import sys, os, binascii
 data_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(data_dir, 'lib'))
 import bitcoin as btc
-import binascii
 
-btc_sk = btc.b58check_to_bin('L5cFx7NvWSWidwNtKnBQw5VrguTax2dGYajuz12S81UhX2ewRehH')
 data = 'hello world' # something to sign
+btc_sk = btc.b58check_to_bin('L5cFx7NvWSWidwNtKnBQw5VrguTax2dGYajuz12S81UhX2ewRehH')
+btc_pk = btc.privkey_to_pubkey(btc_sk)
 btc_sig = btc.ecdsa_sign(data, btc_sk)
 
-# send stuff to server, without sending the btc_pk
+print('the data is {0}'.format(data))
+print('the sk is {0}'.format(btc_sk.encode('hex')))
+print('the pk is {0}'.format(btc_pk.encode('hex')))
+print('the sig is {0}'.format(btc_sig.encode('hex')))
 
-# TODO: figure out btc_pk from only the btc_sig
-btc_pk = btc.privkey_to_pubkey(btc_sk)
-
-print('the sk is {0}', btc_sk)
-print('the pk is {0}', btc_pk)
-print('the data is {0}', data)
-
-
-if not btc.ecdsa_verify(
-        data,
-        btc_sig,
-        btc_pk):
-    print('failed')
-else:
+if btc.ecdsa_verify(data, btc_sig, btc_pk):
     print('success')
+else:
+    print('failed')
